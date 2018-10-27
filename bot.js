4af2eaa27e8d129b3c939f4b2d0686524d608148
iiTW12-Toxic <44296866+iiTW12-Toxic@users.noreply.github.com>
2018-10-27 16:45:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -742,19 +742,9 @@ client.on("guildMemberAdd", (member) => {
         })
     })
 });
-client.on('message', message => {
-
-
-           if (message.content.startsWith(prefix + "id")) {
-           if (message.channel.id !== "502174789985763338") return;
-            if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات ❌`);
-
-                message.guild.fetchInvites().then(invs => {
-      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-      var moment = require('moment');
-      var args = message.content.split(" ").slice(1);
+ client.on('message', message => {
+    if (message.content.startsWith("#id")) {
+var args = message.content.split(" ").slice(1);
 let user = message.mentions.users.first();
 var men = message.mentions.users.first();
  var heg;
@@ -771,22 +761,13 @@ var mentionned = message.mentions.members.first();
      h = message.member
  }
         moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-
-    .setColor("#0a0909")
- .setThumbnail(message.author.avatarURL)
-.addField(': تاريخ دخولك للديسكورد',` \`${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} \`**\n ${moment(heg.createdTimestamp).fromNow()}**` ,true)
-.addField(': تاريخ دخولك لسيرفرنا', `\`${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')}  \` **\n ${moment(h.joinedAt).fromNow()} **`, true)
-.addField(` :لقد قمت بدعوة `, ` ${inviteCount} `)
-
-
-.setFooter(message.author.username, message.author.avatarURL)
-    message.channel.sendEmbed(id);
-})
-}
-
-
-
-     });
+var id = new  Discord.RichEmbed()
+.setColor("RANDOM")
+.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
+.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
+.addField(`: الرتب`, `${message.guild.members.get(h.id).roles.map(r => `\`${r.name}\``).slice(1).join('\n') || 'لايوجد رتب'}`,true)                                                    
+.setThumbnail(heg.avatarURL);
+message.channel.send(id)
+}       });
 
 client.login(process.env.BOT_TOKEN);
